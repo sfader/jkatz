c87ecad3b7156609a1e6dab7305e876ede16e0c2
sfader <sfader@gmail.com>
2014-02-07 15:15:52 -0500

    set up JS function, img paths not correct

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function wagTail()
+  {
+    if (document.getElementById("kat").src=="/ck1.png"){
+      document.getElementById("kat").src="../ck2.png";
+    }
+    else
+    {
+      document.getElementById("kat").src="../ck1.png";
+    }
+  }
