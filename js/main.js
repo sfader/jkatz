af4f28e4c54c8e4c7b1e9f39ef75735c0eb97bbc
sfader <sfader@gmail.com>
2014-02-07 16:06:41 -0500

    wagTail() plays purr.mp3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+var purrA = new Audio('purr.mp3');
+
 function wagTail()
   {
       document.getElementById("kat").src="../jkatz/ck2.png";
+      purrA.play();
       window.setTimeout(wagTailBack,500);
   }
 
